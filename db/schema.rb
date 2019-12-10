fb51246cd9c126cda8ae4bf1d45db6821775ecf7
OlukaDenis <olukadeno@gmail.com>
2019-12-10 14:57:11 +0300

    Added the validations to the User and Micropost models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_091158) do
+ActiveRecord::Schema.define(version: 2019_12_10_092450) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
